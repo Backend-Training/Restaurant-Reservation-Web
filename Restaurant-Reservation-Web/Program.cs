91d7bd7372603d6124e9be61e14cc74391e910e2
amrbadran <badran.amr2003@gmail.com>
2025-10-06 17:35:52 +0300

    Feature: CRUD Operations For Reservations

--- Restaurant-Reservation-Web/Program.cs
+++ Restaurant-Reservation-Web/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant_Reservation_Web.Endpoints;
+using Restaurant_Reservation_Web.Services;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Repositories;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IReservationService, ReservationService>();
+builder.Services.AddScoped<IRepository<Reservation>, EfRepository<Reservation>>();
+
+var app = builder.Build();
+
+app.MapReservationsEndPoints();
+
+app.Run();
